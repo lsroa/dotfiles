2168b7d501da58423a99595c8f438905bc4e7d30
Luis Roa Castro <luis.roa@mac.local>
2022-06-16 15:03:09 +0200

    feat(build): Add tmux config

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 ln -sf $(pwd)/init.lua $HOME/.config/nvim/init.lua
+ln -sf $(pwd)/.tmux.conf $HOME/.tmux.conf
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
 	ln -sf $(pwd)/lua $HOME/.config/nvim/lua
