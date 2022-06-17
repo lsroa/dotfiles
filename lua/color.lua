73c7392751a08bfba3d4b9ecef20245cf3e9bce3
Luis Roa Castro <luis.roa@mac.local>
2022-06-17 09:47:07 +0200

    feat(colors): Change diffs colors in diff view

--- lua/color.lua
+++ lua/color.lua
@@ -1,12 +1,24 @@
 -- require 'github-theme'.setup {}
 -- vim.cmd([[ colorscheme material]])
+-- local cp = require 'catppuccin.api.colors'.get_colors()
+local cat = require 'catppuccin'
+cat.setup {
+	integrations = {
+		neogit = true
+	}
+}
+cat.remap({
+	DiffAdd = {
+		bg = '#244032'
+	},
+	DiffDelete = {
+		bg = '#462c32'
+	}
+})
+
 vim.cmd([[colorscheme catppuccin]])
 
-local hl = function(group, opt)
-	vim.cmd(":hi clear " .. group)
-	vim.api.nvim_set_hl(0, group, opt)
-end
-
--- hl("goTSType", {
--- 	fg = 'orange'
--- })
+-- local hl = function(group, opt)
+-- 	vim.cmd(":hi clear " .. group)
+-- 	vim.api.nvim_set_hl(0, group, opt)
+-- end
