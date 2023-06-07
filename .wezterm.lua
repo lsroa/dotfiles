36d47fea656ee43a87ce7f5f61f390f35086f8d8
Luis Roa <roa234@gmail.com>
2023-06-07 11:46:30 +0200

    Remove gradient and add blink cursor

--- .wezterm.lua
+++ .wezterm.lua
@@ -4,24 +4,22 @@ return {
 	color_scheme = "Catppuccin Mocha",
 	font = wezterm.font("Comic mono"),
 	colors = {
-		cursor_bg = '#f7df1e',
+		cursor_bg = '#a6e3a1',
 	},
+	default_cursor_style = "BlinkingBlock",
+	cursor_blink_rate = 250,
+	animation_fps = 1,
 	-- cell_width = 0.9,
 	enable_tab_bar = false,
 	font_size = 19.0,
-	window_background_opacity = 0.9,
-	window_background_gradient = {
-		orientation = "Vertical",
-		colors = {
-			'#121212',
-			'#121212',
-			'#121212',
-			'#121212',
-			'#141E30',
-			'#141E30',
-			'#243B55',
-		},
-		interpolation = "Basis",
-		blend = 'Rgb'
-	}
+	window_background_opacity = 0.95,
+	-- window_background_gradient = {
+	-- 	orientation = "Vertical",
+	-- 	colors = {
+	-- 		-- '#121212',
+	-- 		'#000000',
+	-- 	},
+	-- 	interpolation = "Basis",
+	-- 	blend = 'Rgb'
+	-- }
 }
