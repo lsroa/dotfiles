d573f358aec75be13a355fe6a6e94bb65327fc0d
Luis Roa <roa234@gmail.com>
2023-07-31 09:49:22 +0200

    Use telescope as ui select

--- lua/lsroa/plugins/telescope.lua
+++ lua/lsroa/plugins/telescope.lua
@@ -2,9 +2,15 @@ return {
 	'nvim-telescope/telescope.nvim',
 	dependencies = {
 		'nvim-lua/plenary.nvim',
+		'nvim-telescope/telescope-ui-select.nvim'
 	},
 	config = function()
 		require 'telescope'.setup {
+			extensions = {
+				["ui-select"] = {
+					require("telescope.themes").get_dropdown {}
+				}
+			},
 			defaults = {
 				layout_strategy = "horizontal",
 				path_display = { "truncate" },
@@ -25,5 +31,6 @@ return {
 				}
 			},
 		}
+		require("telescope").load_extension("ui-select")
 	end
 }
