121a37103669285f70f84690f0adc36288b3e8df
Luis Roa <roa234@gmail.com>
2024-04-28 12:32:09 +0200

    Remove luasnip

--- lua/lsroa/autocmp.lua
+++ lua/lsroa/autocmp.lua
@@ -1,23 +1,13 @@
-require("luasnip.loaders.from_vscode").lazy_load()
-
 local has_words_before = function()
 	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
 	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 end
 
 -- Edit Snippets
-vim.api.nvim_create_user_command("LuaSnipEdit", function() require('luasnip.loaders').edit_snippet_files({}) end, {})
 
 local cmp = require 'cmp'
-local luasnip = require 'luasnip'
 
 cmp.setup({
-	snippet = {
-		-- Snippet engine
-		expand = function(args)
-			require('luasnip').lsp_expand(args.body)
-		end,
-	},
 	window = {
 		completion = cmp.config.window.bordered(),
 		documentation = cmp.config.window.bordered(),
@@ -27,8 +17,6 @@ cmp.setup({
 		["<Down>"] = cmp.mapping(function(fallback)
 			if cmp.visible() then
 				cmp.select_next_item()
-			elseif luasnip.expand_or_jumpable() then
-				luasnip.expand_or_jump()
 			elseif has_words_before() then
 				cmp.complete()
 			else
@@ -38,8 +26,6 @@ cmp.setup({
 		["<Up>"] = cmp.mapping(function(fallback)
 			if cmp.visible() then
 				cmp.select_prev_item()
-			elseif luasnip.jumpable(-1) == 1 then
-				luasnip.jump(-1)
 			else
 				fallback()
 			end
