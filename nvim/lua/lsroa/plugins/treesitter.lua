39eab854db925bfb0f9e12cff621d58f6681265b
Luis Roa <roa234@gmail.com>
2025-09-03 10:44:56 +0200

    Add context TS

--- nvim/lua/lsroa/plugins/treesitter.lua
+++ nvim/lua/lsroa/plugins/treesitter.lua
@@ -1,11 +1,17 @@
 return {
   'nvim-treesitter/nvim-treesitter',
   build = ':TSUpdate',
+  dependencies = {
+    'nvim-treesitter/nvim-treesitter-context',
+  },
   config = function()
     require 'nvim-treesitter.configs'.setup {
       highlight = {
         enable = true,
       },
+      treesitter_context = {
+        enable = true,
+      },
     }
-  end
+  end,
 }
