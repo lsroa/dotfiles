8c1f6e8d2760f4ad52af47e7065930c4b97bd309
Luis Roa <roa234@gmail.com>
2025-12-05 12:10:08 +0100

    Disabled ai plugins

--- nvim/lua/lsroa/plugins/ai.lua
+++ nvim/lua/lsroa/plugins/ai.lua
@@ -7,6 +7,7 @@ return {
   },
   {
     "olimorris/codecompanion.nvim",
+    enabled = false,
     config = function()
       require("codecompanion").setup({
         extensions = {
@@ -16,34 +17,43 @@ return {
               show_result_in_chat = true, -- Show mcp tool results in chat
               make_vars = true,           -- Convert resources to #variables
               make_slash_commands = true, -- Add prompts as /slash commands
-            }
+            },
+            enabled = false,
           }
         },
         strategies = {
           chat = {
-            adapter = "deepseek",
+            adapter = "gemini_cli",
           },
           inline = {
-            adapter = "deepseek",
+            adapter = "gemini_cli",
           }
         },
         adapters = {
-          deepseek = function()
-            return require("codecompanion.adapters").extend("openai_compatible", {
-              name = "deepseek",
-              env = {
-                url = "http://localhost:52415",
-                chat_url = "/v1/chat/completions",
-              },
-              schema = {
-                model = {
-                  default = "deepseek-coder-v2-lite",
-                }
-              },
-            })
-          end,
-        },
+          http = {
+            deepseek = function()
+              return require("codecompanion.adapters").extend("openai_compatible", {
+                name = "deepseek",
+                env = {
+                  url = "http://localhost:52415",
+                  chat_url = "/v1/chat/completions",
+                },
+                schema = {
+                  model = {
+                    default = "deepseek-coder-v2-lite",
+                  }
+                },
+              })
+            end,
+          },
+          acp = {
+            gemini_cli = function()
+              return require("codecompanion.adapters").extend("gemini_cli")
+            end,
+          }
+        }
       })
+      vim.keymap.set("n", "<M-l>", "<cmd>CodeCompanionChat<cr>", { noremap = true, silent = true })
     end,
     dependencies = {
       "nvim-lua/plenary.nvim",
@@ -55,9 +65,85 @@ return {
     dependencies = {
       "nvim-lua/plenary.nvim",
     },
+    enabled = false,
     build = "npm install -g mcp-hub@latest",
     config = function()
       require("mcphub").setup()
     end
-  }
-}
+  },
+  {
+    "folke/sidekick.nvim",
+    opts = {
+      cli = {
+        mux = {
+          backend = "zellij",
+          enabled = true,
+        },
+      },
+    },
+    keys = {
+      {
+        "<tab>",
+        function()
+          -- if there is a next edit, jump to it, otherwise apply it if any
+          if not require("sidekick").nes_jump_or_apply() then
+            return "<Tab>" -- fallback to normal tab
+          end
+        end,
+        expr = true,
+        desc = "Goto/Apply Next Edit Suggestion",
+      },
+      {
+        "<c-.>",
+        function() require("sidekick.cli").toggle() end,
+        desc = "Sidekick Toggle",
+        mode = { "n", "t", "i", "x" },
+      },
+      {
+        "<leader>aa",
+        function() require("sidekick.cli").toggle() end,
+        desc = "Sidekick Toggle CLI",
+      },
+      {
+        "<leader>as",
+        function() require("sidekick.cli").select() end,
+        -- Or to select only installed tools:
+        -- require("sidekick.cli").select({ filter = { installed = true } })
+        desc = "Select CLI",
+      },
+      {
+        "<leader>ad",
+        function() require("sidekick.cli").close() end,
+        desc = "Detach a CLI Session",
+      },
+      {
+        "<leader>at",
+        function() require("sidekick.cli").send({ msg = "{this}" }) end,
+        mode = { "x", "n" },
+        desc = "Send This",
+      },
+      {
+        "<leader>af",
+        function() require("sidekick.cli").send({ msg = "{file}" }) end,
+        desc = "Send File",
+      },
+      {
+        "<leader>av",
+        function() require("sidekick.cli").send({ msg = "{selection}" }) end,
+        mode = { "x" },
+        desc = "Send Visual Selection",
+      },
+      {
+        "<leader>ap",
+        function() require("sidekick.cli").prompt() end,
+        mode = { "n", "x" },
+        desc = "Sidekick Select Prompt",
+      },
+      -- Example of a keybinding to open Claude directly
+      {
+        "<leader>ac",
+        function() require("sidekick.cli").toggle({ name = "claude", focus = true }) end,
+        desc = "Sidekick Toggle Claude",
+      },
+    },
+  } }
