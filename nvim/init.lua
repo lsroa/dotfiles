ca18118915b5328d732f9c9411cf987c40621963
Luis Roa <roa234@gmail.com>
2025-12-05 12:12:41 +0100

    Add Oil.nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -145,7 +145,36 @@ require('lazy').setup({
     end
   },
   { 'itchyny/vim-qfedit',  ft = 'qf' },
-  { 'stevearc/oil.nvim',   config = function() require('oil').setup() end },
+  {
+    'stevearc/oil.nvim',
+    config = function()
+      require('oil').setup(
+        {
+          keymaps = {
+            ["g?"] = { "actions.show_help", mode = "n" },
+            ["<CR>"] = "actions.select",
+            ["<C-s>"] = { "actions.select", opts = { vertical = true } },
+            ["<C-t>"] = { "actions.select", opts = { tab = true } },
+            ["<C-p>"] = "actions.preview",
+            ["<C-c>"] = { "actions.close", mode = "n" },
+            ["-"] = { "actions.parent", mode = "n" },
+            ["_"] = { "actions.open_cwd", mode = "n" },
+            ["`"] = { "actions.cd", mode = "n" },
+            ["~"] = { "actions.cd", opts = { scope = "tab" }, mode = "n" },
+            ["gs"] = { "actions.change_sort", mode = "n" },
+            ["gx"] = "actions.open_external",
+            ["g."] = { "actions.toggle_hidden", mode = "n" },
+            ["g\\"] = { "actions.toggle_trash", mode = "n" },
+          },
+        }
+      )
+    end
+  },
+  {
+    "ibhagwan/fzf-lua",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = {}
+  },
   {
     'rmagatti/auto-session',
     lazy = false,
