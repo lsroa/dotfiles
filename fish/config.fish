77a4ce75d8c9d9a3e7d5af972e093fe5ccb07d38
Luis Roa <roa234@gmail.com>
2025-12-13 16:48:20 +0100

    Add bun, go util for fetching and checking out

--- fish/config.fish
+++ fish/config.fish
@@ -1,22 +1,36 @@
 fish_vi_key_bindings
 set fish_greeting
 
+alias v='nvim'
 set -Ux  GIT_EDITOR "nvim"
 set -Ux  EDITOR "nvim"
 
-alias v='nvim'
-alias vv='/usr/local/bin/nvim'
-alias vit='git diff --name-only master | xargs /usr/local/bin/nvim -p'
+set -Ux XDG_CONFIG_HOME "$HOME/.config"
 alias cat='bat'
 alias x="exit"
 alias gg="lazygit"
 alias c="clear"
 
 
-alias dcu="docker compose up -d"
+alias dcu="docker compose up"
 alias dcd="docker compose down"
 alias dl="docker logs -f"
-alias activate="source venv/bin/activate.fish"
+
+function go
+  if not git checkout $argv[1]
+    git fetch origin $argv[1] && git checkout $args
+  end
+end
+
+function activate
+  if test -e "venv"
+    source "./venv/bin/activate.fish"
+  end
+
+  if test -e ".venv"
+    source "./.venv/bin/activate.fish"
+  end
+end
 
 pyenv init - fish | source
 zoxide init fish | source
@@ -28,3 +42,13 @@ end
 function reload 
 	source ~/.config/fish/config.fish
 end
+
+function gen_uuid
+  uuidgen | string lower | tr -d '\n' | pbcopy
+end
+
+source ~/.invoke-completion.sh
+
+# bun
+set --export BUN_INSTALL "$HOME/.bun"
+set --export PATH $BUN_INSTALL/bin $PATH
